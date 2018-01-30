2127179749184b6b5d1a34c157dd86c43e220f5d
gallyjiang <31274135+gallyjiang@users.noreply.github.com>
2018-01-30 14:10:13 +0800

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 __author__ = 'gally'
-import socket,hashlib,os,json,time
+import socket,hashlib,os,json,time,sys,progressbar
 class Ftpclient(object):
     def __init__(self):
         self.client = socket.socket()
@@ -11,6 +11,7 @@ class Ftpclient(object):
         put [filename] 上传文件
         cd [path] 切换路劲
         dir 列出文件和目录
+        pwd 显示当前目录
         exit 退出
         help 帮助文档
         '''
@@ -43,50 +44,69 @@ class Ftpclient(object):
     def interactive(self, *args):
         print('********WELCOME********')
         while True:
-            msg = input('>>:').strip()
-            if len(msg) == 0:continue
+            current_directory = self.client.recv(1024).decode().strip()
+            self.client.send(b'asdf') #防止粘包
+            full_directory = self.client.recv(1024).decode().strip()
+            msg = input('%s:'%current_directory).strip()
+            if len(msg) == 0:
+                continue
             cmd = msg.split()[0]
             if hasattr(self,cmd):
                 func = getattr(self,cmd)
                 func(msg)
             else:
                 self.command_list()
+                self.client.send('1'.encode('utf-8')) #结束循环
 
     def put(self,*args):
         cmd_split = args[0].split()
         if len(cmd_split) > 1:
             filename = cmd_split[1]
             if os.path.isfile(filename):
+                self.client.send('0'.encode('utf-8'))
                 file_size = os.stat(filename).st_size
-
                 file_dict = json.dumps({'action': 'put', 'filename': filename, 'file_size': file_size})
                 self.client.send(file_dict.encode('utf-8'))
-                if self.client.recv(1024).decode() == '0':
+                return_code = self.client.recv(1024).decode()
+                if return_code == '0':
                     f = open(filename,'rb')
+                    i = 0
                     for line in f:
                         self.client.send(line) #按行发送文件内容
+                        # a = len(line)/file_size
+                        # i += a
+                        # rate = int(i*100)
+                        # s1 = '\r[%s%s]%d%%'%('*'*rate,' '*(100-rate),rate)
+                        # sys.stdout.write(s1)
+                        # sys.stdout.flush()
                     f.close()
-                    print(self.client.recv(1024).decode()) #接受完成信号
-                elif self.client.recv(1024).decode() == '1':
+                    print('\n' + self.client.recv(1024).decode()) #接受完成信号
+                elif return_code == '1':
                     print('超出文件大小')
+
             else:
                 print('文件不存在，请重新输入！')
+                self.client.send('1'.encode('utf-8'))
 
         else:
             print('语法错误！')
-            time.sleep(3)
+            time.sleep(0.5)
             self.command_list()
+            self.client.send('1'.encode('utf-8'))
 
     def get(self,*args):
         cmd_split = args[0].split()
         if len(cmd_split) > 1:
+            self.client.send('0'.encode('utf-8'))
             filename = cmd_split[1]
             file_dict = json.dumps({'action':'get','filename':filename})
             self.client.send(file_dict.encode('utf-8'))
-            file_size = self.client.recv(1024).decode()
-            if file_size == '1':
+            return_code = self.client.recv(1024).decode()
+            if return_code == '1':
                 print('文件不存在！')
+
             else:
+                file_size = self.client.recv(1024).decode()
                 totel_file_size = int(file_size)
                 size = 0
                 self.client.send(b'1234')
@@ -106,12 +126,55 @@ class Ftpclient(object):
 
         else:
             print('语法错误！')
-            time.sleep(3)
+            time.sleep(0.5)
             self.command_list()
-    def cd(self):
-        pass
+            self.client.send('1'.encode('utf-8'))
+
+    def cd(self,*args):
+        cmd_split = args[0].split()
+        if len(cmd_split) > 1:
+            self.client.send('0'.encode('utf-8'))
+            file_dict = json.dumps({'action': 'cd'})
+            self.client.send(file_dict.encode('utf-8'))
+            path = cmd_split[1]
+            self.client.send(path.encode('utf-8'))
+            print(self.client.recv(1024).decode())
+            # error_code = self.client.recv(1024)
+            # if error_code == '2':
+            #     print('拒绝访问')
+            # else:
+            #     pass
+        else:
+            print('语法错误！')
+            time.sleep(0.5)
+            self.command_list()
+
     def dir(self,*args):
-        pass
+        cmd_split = args[0].split()
+        if len(cmd_split) == 1:
+            file_dict = json.dumps({'action': 'cd'})
+            self.client.send(file_dict.encode('utf-8'))
+
+        else:
+            print('语法错误')
+            time.sleep(0.5)
+            self.command_list()
+
+
+    def pwd(self,*args):
+        cmd_split = args[0].split()
+        if len(cmd_split) == 1:
+            self.client.send('0'.encode('utf-8'))
+            file_dict = json.dumps({'action':'pwd'})
+            self.client.send(file_dict.encode('utf-8'))
+            pwd_dir = self.client.recv(1024).decode()
+            print(pwd_dir)
+        else:
+            print('语法错误')
+            time.sleep(0.5)
+            self.command_list()
+            self.client.send('1'.encode('utf-8'))
+
     def exit(self,*args):
         # if len(args) == 1:
         #     return 1
@@ -124,5 +187,5 @@ class Ftpclient(object):
 
 
 client = Ftpclient()
-client.connect('localhost',9908)
+client.connect('localhost',9905)
 client.main_menu()
